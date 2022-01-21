721df99e6f279eae3aea19930daa2c24bd7ca41d
bglepark <hspark.qa@gmail.com>
2022-01-21 23:22:16 +0900

    10845_큐_문제

---
+++ 백준 알고리즘/큐/10845_큐/10845_큐.py
@@ -0,0 +1,50 @@
+# 정수를 저장하는 큐를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+#
+# 명령은 총 여섯 가지이다.
+#
+# push X: 정수 X를 큐에 넣는 연산이다.
+# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 큐에 들어있는 정수의 개수를 출력한다.
+# empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+# front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+
+
+import sys
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N):
+    a = sys.stdin.readline().strip()
+
+    if a.startswith('push'): #push 구현
+        stack.append(a.split()[-1])
+
+    elif a == 'front': #top 구현
+        if stack == []:
+            print(-1)
+        else:
+            print(stack[0])
+
+    elif a == 'back':
+        if stack ==[]:
+            print(-1)
+        else:
+            print(stack[-1])
+
+    elif a == 'pop': #pop 구현
+        if stack == []:
+            print(-1)
+        else:
+            print(stack[0])
+            del stack[0]
+
+    elif a == 'size': #size 구현
+        print(len(stack))
+
+    elif a == 'empty': #empty 구현
+        if stack == []:
+            print(1)
+        else:
+            print(0)
