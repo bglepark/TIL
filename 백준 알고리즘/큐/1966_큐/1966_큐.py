57581cbee5506f67cc1a1492fce93b2c75faf8e5
bglepark <hspark.qa@gmail.com>
2022-01-23 17:05:58 +0900

    1966_큐_코드

--- 백준 알고리즘/큐/1966_큐/1966_큐.py
+++ 백준 알고리즘/큐/1966_큐/1966_큐.py
@@ -9,3 +9,44 @@
 # 여러분이 할 일은, 현재 Queue에 있는 문서의 수와 중요도가 주어졌을 때, 어떤 한 문서가 몇 번째로 인쇄되는지 알아내는 것이다.
 # 예를 들어 위의 예에서 C문서는 1번째로, A문서는 3번째로 인쇄되게 된다.
 
+# 첫 줄에 테스트케이스의 수가 주어진다. 각 테스트케이스는 두 줄로 이루어져 있다.
+#
+# 테스트케이스의 첫 번째 줄에는 문서의 개수 N(1 ≤ N ≤ 100)과,
+# 몇 번째로 인쇄되었는지 궁금한 문서가 현재 Queue에서 몇 번째에 놓여 있는지를 나타내는 정수 M(0 ≤ M < N)이 주어진다.
+# 이때 맨 왼쪽은 0번째라고 하자. 두 번째 줄에는 N개 문서의 중요도가 차례대로 주어진다.
+# 중요도는 1 이상 9 이하의 정수이고, 중요도가 같은 문서가 여러 개 있을 수도 있다.
+
+import sys
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n , idx = map(int , sys.stdin.readline().split())
+    q = list(map(int , sys.stdin.readline().split()))
+    count = 0
+    while q: #q가 빈 리스트가 아닐때
+        max_q = max(q)
+        first = q[0]
+        del q[0]
+
+        idx -=1
+
+
+        if max_q == first: #첫 숫자가 가장 큰 숫자일 경우
+            count += 1
+            if idx == -1:
+                print(count)
+                break
+
+        elif max_q != first:
+            q.append(first)
+            if idx ==-1:
+                idx = len(q)-1
+
+
+
+
+
+
+
+
+
