edf4990730314402151b94d8a003dacd4ec31e12
bglepark <96849924+bglepark@users.noreply.github.com>
2022-02-13 19:10:47 +0900

    Delete workspace_django directory

--- workspace_django/myboard/myboard/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MyBoard , MyMember
-
-admin.site.register(MyBoard)
-
-admin.site.register(MyMember)
--- workspace_django/tags/tags/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# index에 있는 name이라는 변수 안에 본인 이름을 전달해준다
-# render : 해당 요청에 맞게 그려준다. 요청을 받으면 해당 index.html 템플릿을 찾아서 거기에 name에 변수를 넣어줘서 그려줘라
-# server-sider-rendering
-def index(request):
-    return render(request , 'index.html' , {'name': 'hyunsoo'})
